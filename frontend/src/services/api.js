ee581bec4bb7ee587d668c76c1a7ec4dca2e7134
Mayur <mayurtonge14@gmail.com>
2026-05-18 23:36:17 +0530

    Production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080"
+    // baseURL: "http://localhost:8080"
+    baseURL: "https://inventra-platform.onrender.com"
+
 });
 
 // Attach JWT token automatically
